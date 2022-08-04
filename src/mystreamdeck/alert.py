8e1a4ef4dbae6612383fd958817996e74e2d5a16
ktat <ktat@wano.co.jp>
2022-08-04 20:17:48 +0900

    add retry_interval config

--- src/mystreamdeck/alert.py
+++ src/mystreamdeck/alert.py
@@ -12,6 +12,7 @@ class Alert:
 
     _check_function = None
     _check_interval = 300
+    _retry_interval = 60
     _previous_checke_time = 0
     in_alert = False
 
@@ -19,20 +20,27 @@ class Alert:
         alert_key_config = {}
         if config.get("check_interval"):
             self._check_interval = config.get("check_interval")
+        if config.get("retry_interval"):
+            self._retry_interval = config.get("retry_interval")
         if config.get("key_cofnig"):
             alert_key_config = config.get("key_cofnig")
             
         self._previous_checke_time = 0
         self.mydeck = mydeck
-        mydeck.add_alert_key_conf(alert_key_config)
 
     def set_check_func(self, f):
         self._check_function = f
 
+    def set_conf(self, conf):
+        self.mydeck.key_config()['~ALERT'] = conf
+
     def start(self):
         while True:
             current = time.time()
-            if current - self._previous_checke_time > self._check_interval:
+            interval = self._check_interval
+            if self.in_alert:
+                interval = self._retry_interval
+            if current - self._previous_checke_time > interval:
                 self._previous_checke_time = current
                 if self._check_function():
                     self.mydeck.handler_alert()
