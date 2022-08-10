b90e87d2abaf0d5741fba4aa893cd962d1cf600d
ktat <ktat@wano.co.jp>
2022-08-11 07:49:13 +0900

    change start and stop command to toggle comamnd(start or stop)

--- src/mystreamdeck/stop_watch.py
+++ src/mystreamdeck/stop_watch.py
@@ -12,20 +12,12 @@ Y = 100
 
 class StopWatch(AppBase):
     _key_conf = {
-        "start": {
-            "app_command": "MyStreamDeckStopWatchStart",
-            "image": "./src/Assets/stopwatch.png",
-            "label": "Stop Watch",
-        },
-        "stop": {
-            "app_command": "MyStreamDeckStopWatchStop",
-            "image": "./src/Assets/stopwatch.png",
-            "label": "Stop Watch",
-        }
+        "app_command": "MyStreamDeckStopWatchToggle",
+        "image": "./src/Assets/stopwatch.png",
+        "label": "Stop Watch",
     }
     key_command = {
-        "MyStreamDeckStopWatchStart": lambda app: app.do_start(),
-        "MyStreamDeckStopWatchStop":  lambda app: app.do_stop(),
+        "MyStreamDeckStopWatchToggle": lambda app: app.toggle_count(),
     }
 
     def __init__(self, mydeck, option={}):
@@ -33,35 +25,46 @@ class StopWatch(AppBase):
 
     # setup key configuration
     def key_setup(self):
-        for page in self.page_key.keys():
-            key = self.page_key[page]
-            self.mydeck.set_key_conf(page, key, self._key_conf["start"])
+        page = self.mydeck.current_page()
+        key = self.page_key.get(page)
+        if key is not None:
+            self.mydeck.set_key_conf(page, key, self._key_conf)
 
     def do_start(self):
-        print("do_start")
-        self.stop = False        
+        self.stop = False
         page = self.mydeck.current_page()
-        key = self.page_key[page]
-        self.mydeck.set_key_conf(page, key, self._key_conf["stop"])
-        t = threading.Thread(target=lambda : self.count_up(key), args=())
-        t.start()
+        key = self.page_key.get(page)
+        if key is not None:
+            self.mydeck.set_key_conf(page, key, self._key_conf)
+            t = threading.Thread(target=lambda : self.count_up(key), args=())
+            t.start()
 
 
     def count_up(self, key):
         t = time.time()
         font = ImageFont.truetype(self.mydeck.font_path, 35)
         while True:
+            self.in_working = True
+
             time.sleep(0.1)
+
+            # exit when main process is finished
+            if self.check_to_stop():
+                self.stop_app()
+                self.key_setup()
+                break
+
             if self.mydeck.current_page() in self.page_key.keys():
                 im = Image.new('RGB', (X, Y), (0, 0, 0))
                 draw = ImageDraw.Draw(im)
                 n = "{0:02.2f}".format(int((time.time() - t) * 100) / 100)
                 draw.text((0,45), text=n, font = font, andhor="ms", fill="white")
                 self.mydeck.update_key_image(key, self.mydeck.render_key_image(im, "STOP/START", "black"))
-            if self.stop or self.mydeck._exit:
-                break
         sys.exit()
-            
-    def do_stop(self):
-        self.stop = True
-        self.key_setup()
+
+    def toggle_count(self):
+        if self.in_working:
+            self.stop = True
+        else:
+            self.do_start()
+
