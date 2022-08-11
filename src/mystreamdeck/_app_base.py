23c53ea7f75b33f559e0edfc1f6867eb102069b2
ktat <ktat@wano.co.jp>
2022-08-11 23:29:42 +0900

    move code of 01_background_app_base.py => 00_app_base.py rename 00_app_base.py => _app_base.py

--- src/mystreamdeck/_app_base.py
+++ src/mystreamdeck/_app_base.py
@@ -112,3 +112,29 @@ class AppBase:
             return True
         else:
             return False
+
+class BackgroundAppBase:
+    use_thread = True
+    # need to stop thread
+    stop = False
+    # sleep time in thread
+    sleep  = 1
+
+    in_working = False
+    is_background_app = True
+
+    def __init__ (self, mydeck, config={}):
+        self.mydeck = mydeck
+
+    def start(self):
+        mydeck = self.mydeck
+        while True:
+            if mydeck.in_alert() is False:
+                self.execute_in_thread()
+
+            if mydeck._exit:
+                break
+
+            time.sleep(self.sleep)
+
+        sys.exit()
