3884c874183190760f6060c1b3272cbb95f62ef3
ktat <ktat.is@gmail.com>
2022-11-25 02:03:01 +0900

    forgot to add app_trigger.py

---
+++ src/mystreamdeck/app_trigger.py
@@ -0,0 +1,40 @@
+import datetime
+import time
+from mystreamdeck import MyStreamDeck, BackgroundAppBase
+
+class AppTrigger(BackgroundAppBase):
+    # if app reuquire thread, true
+    use_thread = True
+    # need to stop thread
+    stop = False
+
+    def __init__(self, mydeck: MyStreamDeck, config: dict = {}):
+        super().__init__(mydeck)
+        self.now = datetime.datetime.now()
+
+    def start(self):
+        while True:
+            current_time = datetime.datetime.now()
+            if current_time.day != self.now.day:
+                for app in self.mydeck.config.apps:
+                    if app.use_day_trigger or app.use_hour_trigger or app.use_minute_trigger:
+                       print("trigger day")
+                       app.trigger.set()
+                       self.now = current_time
+            elif current_time.hour != self.now.hour:
+                for app in self.mydeck.config.apps:
+                    if app.use_hour_trigger or app.use_minute_trigger:
+                       print("trigger hour")
+                       app.trigger.set()
+                       self.now = current_time
+            elif current_time.minute != self.now.minute:
+                for app in self.mydeck.config.apps:
+                    if app.use_minute_trigger:
+                       print("trigger minute")
+                       print(app.trigger)
+                       app.trigger.set()
+                       self.now = current_time
+
+            # sleep untile next munite
+            time.sleep(current_time.second % 60)
+
