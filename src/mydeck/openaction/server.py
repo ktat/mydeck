2c8bd00eef95f5c3a40942bfe884df75194e74cf
Atsushi Kato <ktat.is@gmail.com>
2026-04-23 09:43:40 +0900

    fix(openaction): revert from_token to use rsplit per spec

--- src/mydeck/openaction/server.py
+++ src/mydeck/openaction/server.py
@@ -25,11 +25,8 @@ class KeyContext:
 
     @classmethod
     def from_token(cls, token: str) -> "KeyContext":
-        parts = token.split("|")
-        deck = parts[0]
-        key = int(parts[-1])
-        page = "|".join(parts[1:-1])
-        return cls(deck_serial=deck, page=page, key=key)
+        deck, page, key = token.rsplit("|", 2)
+        return cls(deck_serial=deck, page=page, key=int(key))
 
 
 class OpenActionServer:
